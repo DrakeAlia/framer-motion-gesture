a3fd26b352d7c88005c10f184656bc3fdf882bcb
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-08-22 16:06:00 -0700

    fixed typo with onClick and tried applying other examples

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import { motion } from "framer-motion";
 import "./styles.css";
 
 const App = () => {
+  // const [rotate, setRotate] = React.useState(false)
+  // const [move, setMove] = React.useState(false)
   return (
     <div className="example-container">
       {/* Every element you want to animate you do by adding motion. in the element */}
@@ -23,7 +25,7 @@ const App = () => {
 render(<App />, document.getElementById("root"));
 
 
-// Notes & Different animation motions:
+// Notes & Different animation motions examples:
 
  // If it is true rotate to 360 degrees, if it is false then it should be 0 degrees
     // const [rotate, setRotate] = React.useState(false)
@@ -34,49 +36,49 @@ render(<App />, document.getElementById("root"));
       // initial is the begining state for your element with animation
       // animate={{ x: 100, scale: 1}} initial={{ scale: 0 }}
 
-      // animate={{ 
-        // If rotate state is true then rotate 360 degrees 
-        // If its false, rotate back to 0 degrees
+    // animate={{ 
+       // // If rotate state is true then rotate 360 degrees 
+       // // If its false, rotate back to 0 degrees
         // rotate: rotate ? 360 : 0 }} 
-        // when you click on this div, set rotate to the opposite of what it is
-        // onclick={() => {
+        // // When you click on this div, set rotate to the opposite of what it is
+        // onClick={() => {
         //   setRotate(!rotate)
         // }}
 
-      // animate={{ 
-        // if move is true, we move 200px to the right
-        // if its false then move 200px to the left
+    // animate={{ 
+       // // If move is true, we move 200px to the right
+       // // If its false then move 200px to the left
         // x: move ? 200 : -200 }}
-        // transition - prop gives certain properties that have to do with transition state of the element
-        // delay the animation for 1 second 
+        // // transition - prop gives certain properties that have to do with transition state of the element
+        // // delay the animation for 1 second 
         // transition={{ delay: 1 }}
-        // onclick={() => {
+        // onClick={() => {
         //   setMove(!move)
         // }}
 
-      // animate={{ 
+    // animate={{ 
         // x: move ? 200 : -200 }}
-        // spring, tween, inertia - is a physics animation when you need that kind of effect for your animation
-        // type - is a type of transition were you can determine the duration of it
+        // // spring, tween, inertia - is a physics animation when you need that kind of effect for your animation
+        // // type - is a type of transition were you can determine the duration of it
         // transition={{ type: "tween", duration: 5 }}
-        // onclick={() => {
+        // onClick={() => {
         //   setMove(!move)
         // }}
 
-      // animate={{ 
+    // animate={{ 
         // x: move ? 200 : -200 }}
-        // bounce - adds a "bounce" effect the element
+        // // bounce - adds a "bounce" effect the element
         // transition={{ type: "spring", bounce: 5 }}
-        // onclick={() => {
+        // onClick={() => {
         //   setMove(!move)
         // }}
 
-      // animate={{ 
+    // animate={{ 
         // x: move ? 200 : -200 }}
-        // When you using inertia type you can determine the velocity
-        // the element moves on its own but with some resistance depending on how you set your velocity
+        // // When you using inertia type you can determine the velocity
+        // // the element moves on its own but with some resistance depending on how you set your velocity
         // transition={{ type: "inertia", velocity: 40 }}
-        // onclick={() => {
+        // onClick={() => {
         //   setMove(!move)
         // }}
 
@@ -90,7 +92,7 @@ render(<App />, document.getElementById("root"));
 
       // Adding constraints to how much you can drag the element
       // drag the element only up and down with just "y"
-      // drag only left and right with "x"
+      // drag only left and right with "x" just like we did with "y"
       // <motion.div drag="y"></motion.div>
 
       // You can only drag up to 50px to the left before coming back 
@@ -100,11 +102,11 @@ render(<App />, document.getElementById("root"));
       // Animate were the element is consistently rotating back and fourth
       // Using an array is best because is can be use in a sequential order for something like a rotation
       // <motion.div 
-      // animate={{
+    // animate={{
       //     rotate: [0, 200, 200, 0],
-      // Rotate while moving in the x direction
+      // // Rotate while moving in the x direction
       //     x: [0, 200, 200, 0, -200, -200, 0]
       //   }}
-      // Keep the animation rotating forever for every 3 seconds
+      // // Keep the animation rotating forever for every 3 seconds
       // transition={{ repeat: Infinity, duration: 3 }}
       // ></motion.div>
